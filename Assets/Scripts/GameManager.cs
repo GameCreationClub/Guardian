1a2039cc353b82147db4a234d9d55f8b4fb7c037
Adam Pod <adam.podoxin@gmail.com>
2020-01-01 20:39:08 -0800

    Made entities list private

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public List<Entity> entities;
+    [SerializeField] private List<Entity> entities;
 
     private int currentTurn = 0, currentAction = 0;
     private float amountOfTurnsTaken = 0f;
