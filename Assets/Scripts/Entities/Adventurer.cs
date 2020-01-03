26e26df83f29ef318ceb9bf496848887c6c46b08
Shahan Neda <shahan.neda@gmail.com>
2020-01-03 13:46:04 -0800

    made it so each the camera pans to whoever has the turn

--- Assets/Scripts/Entities/Adventurer.cs
+++ Assets/Scripts/Entities/Adventurer.cs
@@ -4,9 +4,14 @@ public class Adventurer : Entity
 {
     public int mana;
 
+    public void Start()
+    {
+        Camera.main.GetComponent<CameraMovement>().trackedObject = gameObject;
+    }
     public override void MovementTurn()
     {
         print("Adventurer movement");
+        Camera.main.GetComponent<CameraMovement>().trackedObject = gameObject;
     }
 
     public override void AttackTurn()
--- Assets/Scripts/Entities/Guardian.cs
+++ Assets/Scripts/Entities/Guardian.cs
@@ -2,13 +2,11 @@
 
 public class Guardian : Entity
 {
-    public void Start()
-    {
-        Camera.main.GetComponent<CameraMovement>().trackedObject = gameObject;//this is temp later it would make more sense for the game manager to actually set this when needed
-    }
     public override void MovementTurn()
     {
         print("Guardian movement");
+        Camera.main.GetComponent<CameraMovement>().trackedObject = gameObject;
+
     }
 
     public override void AttackTurn()
